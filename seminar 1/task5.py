66f51a4764feda0426380de32f231efdf8c60e54
Janna02 <dendy2@yandex.ru>
2022-12-14 16:08:58 +0300

    task5

--- seminar 1/task5.py
+++ seminar 1/task5.py
@@ -5,4 +5,19 @@
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
 
+print("Введите координату x первой точки")
+x1 = int(input())
+print("Введите координату y первой точки")
+y1 = int(input())
+print("Введите координату x второй точки")
+x2 = int(input())
+print("Введите координату y второй точки")
+y2 = int(input())
 
+def DistanceOfPoints(xOne, yOne,xTwo,yTwo):
+   distance = ((xTwo - xOne) ** 2 + (yTwo - yOne) ** 2) ** (0.5)
+   return distance
+
+print("Расстояние между первой и второй точками  в 2D пространстве:")
+distanceOfPoints = DistanceOfPoints(x1,y1,x2,y2)
+print(distanceOfPoints)
